e52af6bfc516276967d86550812f7b379e6e1271
Adaamss <aademhamdi@gmail.com>
2026-05-06 15:01:11 +0100

    chore: update config and setup for new architecture

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,55 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { router } from "../routes";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 
 export const app = express();
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    },
+    xDownloadOptions: false,
+
+    frameguard: { action: "deny" },
+
+    hidePoweredBy: true,
+
+    noSniff: true,
+
+    xssFilter: true,
+
+    hsts: {
+      maxAge: 15552000, // 180 days
+      includeSubDomains: true,
+    },
+  }),
+);
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 10000,
+    statusCode: 429,
+    message: {
+      success: false,
+      error: "Slow down, Please try again in 5 minutes.",
+      retryAfter: "5 minutes",
+    },
+    legacyHeaders: false, 
+    standardHeaders: "draft-7", 
+  }),
+);
 
 app.get("/", (req, res) => {
   res.status(200).send("hello");
