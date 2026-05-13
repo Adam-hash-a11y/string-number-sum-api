eb55281912f16661ffdac0c93d9225273e63d902
Adaamss <aademhamdi@gmail.com>
2026-05-13 20:43:16 +0100

    chore: remove uneeded middleware

--- src/routes/sumNumber.route.ts
+++ src/routes/sumNumber.route.ts
@@ -2,14 +2,12 @@
     import { sumNumber } from "../controller/sumNumber.controller";
     import { sumNumberMiddleware } from "../middleware/sumNumber.middleware";
     import { authMiddleware } from "../middleware/auth.middleware";
-    import { callLogMiddleware } from "../middleware/callLog.middleware";
 
     export const sumNumberRouter = express.Router();
 
     sumNumberRouter.post(
     "/",
     authMiddleware,
-    callLogMiddleware,
     sumNumberMiddleware,
     sumNumber,
     );
