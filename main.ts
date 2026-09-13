289bb7afebfc1924ffdd50e278a2ac359daf40e7
Adaamss <aademhamdi@gmail.com>
2026-09-13 15:18:06 +0100

    fix: use configured application port

--- main.ts
+++ main.ts
@@ -3,12 +3,15 @@ import { connectDB } from "./src/config/dbConfig";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
   throw new Error("MONGO_URI and JWT_SECRET are required");
 }
 
+const PORT = process.env.PORT || 5003;
+
 connectDB().then(() =>
-  app.listen(5003, () => {
-    console.log("running on http://localhost:5003/");
+  app.listen(PORT, () => {
+    console.log(`running on http://localhost:${PORT}/`);
   }),
 );
