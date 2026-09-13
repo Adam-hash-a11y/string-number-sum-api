fddaf7a568a9537e35892e0c0871917d3d25a952
Adaamss <aademhamdi@gmail.com>
2026-09-13 14:08:44 +0100

    fix: validate required environment variables

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 import { app } from "./app";
 import { connectDB } from "./src/config/dbConfig";
 import dotenv from "dotenv";
+
 dotenv.config();
+if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
+  throw new Error("MONGO_URI and JWT_SECRET are required");
+}
 
 connectDB().then(() =>
   app.listen(5003, () => {
